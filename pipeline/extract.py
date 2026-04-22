48fab10aa89cc31e491b611a3554e8ad03086088
Stephen Donlin <sddonlin@gmail.com>
2026-04-21 21:20:46 -0700

    minor updates

--- pipeline/extract.py
+++ pipeline/extract.py
@@ -1,42 +1,40 @@
+''' 
+Functions to load data and convert to polars lazyframe. 
+'''
+
 import nflreadpy as nfl
 import polars as pl
-from utils import get_dimensions
 
 
-def extract_pbp(seasons: list = None) -> pl.DataFrame:
-    # Extract play by play data from NFLverse
-    # Default = 2025 season, if no list
+def extract_pbp(seasons: list = None) -> pl.LazyFrame:
+    ''' Extract play by play data from NFLverse, default = 2025 season '''
     if seasons:
-        return nfl.load_pbp(seasons)
+        return nfl.load_pbp(seasons).lazy()
     else:
-        return nfl.load_pbp()
+        return nfl.load_pbp().lazy()
 
 
-def extract_schedules(seasons: list = None) -> pl.DataFrame:
-    # Extract schedule data from NFLverse
-    # Default = 2025 season, if no list
+def extract_schedules(seasons: list = None) -> pl.LazyFrame:
+    ''' Extract schedule data from NFLverse, default = 2025 season'''
+    if seasons: 
+        return nfl.load_schedules(seasons).lazy()
+    else: 
+        return nfl.load_schedules().lazy()
+
+
+def extract_participation(seasons: list = None) -> pl.LazyFrame:
+    ''' Extract player participation data from NFLverse default = 2025 season '''
     if seasons:
-        return nfl.load_schedules(seasons)
+        return nfl.load_participation(seasons).lazy()
     else:
-        return nfl.load_schedules()
+        return nfl.load_participation().lazy()
 
 
-def extract_participation(seasons: list = None) -> pl.DataFrame:
-    # Extract player participation data from NFLverse
-    # Default = 2025 season, if no list
-    if seasons:
-        return nfl.load_participation(seasons)
-    else:
-        return nfl.load_participation()
-
-
-def extract_charting(seasons: list = None) -> pl.DataFrame:
-    # Extract charting data from NFLverse
-    # Default = 2025 season, if no list
-    # Data avialable from 2022
+def extract_charting(seasons: list = None) -> pl.LazyFrame:
+    ''' Extract charting data from NFLverse, default = 2025 season, if no list. Data avialable from 2022 ''' 
     if seasons:
         # only pass available seasons (from 2022)
         available_seasons = [s for s in seasons if s >= 2022]
-        return nfl.load_ftn_charting(available_seasons)
+        return nfl.load_ftn_charting(available_seasons).lazy()
     else:
-        return nfl.load_ftn_charting()
+        return nfl.load_ftn_charting().lazy()
--- pipeline/transform.py
+++ pipeline/transform.py
@@ -7,80 +7,59 @@ if TYPE_CHECKING:
 
 
 def load_cols(path: str) -> list:
-    # Returns a dictionary of columns to be used with the data loaded as key
-    # Ex. { play-by-play : ['play_id', 'game_id', 'down', 'distance'...] }
+    ''' Returns a dictionary of columns to be used with the data loaded as key. Ex. { play-by-play : ['play_id', 'game_id', 'down', 'distance'...] } '''
     from pathlib import Path
     file = Path(path).expanduser()
     if file.exists():
         cols = [line for line in file.read_text().splitlines()
-                if line.strip() and not line.startswith('#')]
+                if line.strip() and not line.startswith('#')
+                ]
     
     return cols
 
 def select_dimensions(raw_data: pl.LazyFrame, cols: list) -> pl.LazyFrame:
-    ''' Select a subset of columns from a polars dataframe.'''
-    from polars import col    
-    output = raw_data.select(col(cols))
+    ''' Select a subset of columns from a polars dataframe. '''
+    output = raw_data.select(cols)
     return output
 
-def merge_play(pbp_lf, participation_lf, charting_lf):
-    play_table_data = pl.LazyFrame
-    pbp = pbp_lf.select([ 
-        'game_id', 'play_id', 'yds_gained', 'rush_attempt', 'rush_touchdown',   'run_location', 'run_gap', 'pass_attempt', 'complete_pass', 'pass_touchdown', 'interception', 'air_yards', 'yards_after_catch','pass_location', 'qb_kneel', 'qb_spike', 'qb_dropback', 'qb_scramble', 'sack'
+
+def merge_play_data(pbp_lf, participation_lf, charting_lf) -> pl.LazyFrame:
+    ''' Merges data for the play table '''
+    pbp = pbp_lf.select([ 'game_id', 'play_id', 'yds_gained', 'rush_attempt', 'rush_touchdown', 'run_location', 'run_gap', 'pass_attempt', 'complete_pass', 'pass_touchdown', 'interception', 'air_yards', 'yards_after_catch','pass_location', 'qb_kneel', 'qb_spike', 'qb_dropback', 'qb_scramble', 'sack'
         ])
     
-    charting = charting_lf.select([
-        'nflverse_game_id', 'qb_location', 'is_drop', 'is_drop', 'play_action', 'screen_pass', 'qb_sneak', 'trick_play', 'n_pass_rushers', 'n_blitzers'
-    ])
+    charting = charting_lf.select(['nflverse_game_id', 'qb_location', 'is_drop', 'play_action', 'screen_pass', 'qb_sneak', 'trick_play', 'n_pass_rushers', 'n_blitzers'])
+
+    participation = participation_lf.select(['nflverse_game_id', 'offense_positions', 'route', 'was_pressure', 'defenders_in_box', 'defense_man_zone_type', 'defense_coverage_type'])    
     
-    participation = participation_lf.select([
-        'nflverse_game_id', 'offense_positions', 'route', 'was_pressure', 'defenders_in_box', 'defense_man_zone_type', 'defense_coverage_type'
-        ])
+    result = (pbp.join(charting, left_on = 'game_id', 
+                        right_on = 'nflverse_game_id', how = 'left').join(
+                            participation, left_on = 'game_id', 
+                            right_on = 'nflverse_game_id',how = 'left'
+                            ))
+    return result 
 
 
-def merge_situation(pbp_lf, charting_lf):
-    situation_table_data = pl.LazyFrame
+def merge_situation(pbp_lf, charting_lf) -> pl.LazyFrame: 
+    ''' Merges data for situation table. '''
 
-    pbp = pbp_lf.select([
-        'game_id', 'play_id', 'game_id', 'posteam', 'yardline_100', 'quarter_seconds_remaining', 'down', 'goal_to_go', 'ydstogo'
+    pbp = pbp_lf.select(['game_id', 'play_id', 'game_id', 'posteam', 'yardline_100', 'quarter_seconds_remaining', 'down', 'goal_to_go', 'ydstogo'
         ])
     
     charting = charting_lf.select(['nflverse_game_id','starting_hash'])
 
+    result = (pbp.join(charting, left_on = 'game_id', 
+                       right_on = 'nflverse_game_id', how = 'left'
+                       ))
+    return result
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-    ])
-
-def transform_pbp(raw_pbp: pl.DataFrame):
+def transform_play(raw_pbp: pl.DataFrame):
     path = './data/dimension_definitions/pbp.txt'
     cols = load_cols(path)
     #pbp_df = select_dimensions(raw_pbp, cols) # selects the column
     pbp_lf = raw_pbp.lazy()
     
-    query = (
-        pbp_lf
-        .select(cols)
-        .rename()
-    )
-
 
 
 
